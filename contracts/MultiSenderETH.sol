c21c3e554747201a0eabf94fdd89bbfb2d800196
hoangnh2 <hoangnh5901@gmail.com>
2022-04-14 10:47:56 +0700

    complete MTS ETH

--- contracts/MultiSenderETH.sol
+++ contracts/MultiSenderETH.sol
@@ -1,21 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/utils/Strings.sol";
 
-// TODO fix bug
 contract MultiSenderETH {
     function sendMultiETH(
         address payable[] memory listReceivers,
         uint256[] memory listAmounts
     ) public payable {
         uint256 totalReceivers = listReceivers.length;
-        // require(
-        //     msg.sender.balance >= totalReceivers * msg.value,
-        //     "Total balance not enough"
-        // );
+        uint256 totalAmounts;
         for (uint256 i = 0; i < totalReceivers; i++) {
-            listReceivers[i].transfer(listAmounts[i]);
+            totalAmounts += listAmounts[i];
+        }
+        require(msg.sender.balance >= totalAmounts, "Total balance not enough");
+        require(msg.value == totalAmounts, "Value not enough");
+        for (uint256 i = 0; i < totalReceivers; i++) {
+            (bool success, bytes memory data) = listReceivers[i].call{
+                value: listAmounts[i]
+            }("");
+            delete data;
+            require(
+                success,
+                string(
+                    abi.encodePacked(
+                        "Transaction ",
+                        Strings.toString(i),
+                        " failed"
+                    )
+                )
+            );
         }
     }
 }
-
-// [ "0x20e5f90F0013e59Aa3B0cfeC832815db187b05FA" , "0x627306090abaB3A6e1400e9345bC60c78a8BEf57"    ]
